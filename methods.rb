a54031306166e24e09377d58802269a786922d2b
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-05-28 21:38:44 -0400

    commit tree eb6bde75f4c3f9d23b35a5531ce81da8b8974701

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
-# Your code here!
+def my_function(param)
+  puts "Running my_function"
+  param + 1
+end
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def halve(number)
+  if number.class != Integer
+    return nil
+  end
+
+    return number / 2;
+end
